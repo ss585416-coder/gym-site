2fa257580b0e445a7b8c1bb3fa87cf62e908c29d
ss585416-coder <ss585416@gmail.com>
2026-05-10 14:44:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
-function joinGym(){
-
-  alert("Welcome to Power Gym!");
-
+*{
+  padding:15px;
+  margin-bottom:10px;
+  border-radius:10px;
 }
 
-function sendMessage(event){
+.payment-box{
+  background:#222;
+  padding:25px;
+  border-radius:15px;
+}
 
-  event.preventDefault();
+.payment-box label{
+  display:block;
+  margin:15px 0;
+}
 
-  alert("Message Sent Successfully!");
+.pay-btn{
+  width:100%;
+  padding:14px;
+  border:none;
+  background:#ffb703;
+  font-size:18px;
+  font-weight:bold;
+  border-radius:10px;
+  cursor:pointer;
+}
+
+.about,
+.contact{
+  padding:80px 8%;
+  text-align:center;
+}
+
+.about p,
+.contact p{
+  font-size:18px;
+  margin-top:10px;
+}
+
+footer{
+  text-align:center;
+  padding:20px;
+  background:#000;
+}
+
+@media(max-width:768px){
+
+  nav{
+    flex-direction:column;
+    gap:15px;
+  }
+
+  .hero h2{
+    font-size:35px;
+  }
 
 }
